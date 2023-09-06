f64956b06c0ec3344669e4d7a5437453217767c7
ZHU JUNHAO <realzhujunhao@gmail.com>
2023-09-06 14:34:01 +0900

    upgrade cxxopt

--- src/cxxopts/src/example.cpp
+++ src/cxxopts/src/example.cpp
@@ -21,36 +21,51 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 
 */
-
-#include <iostream>
-
 #include "cxxopts.hpp"
 
-int main(int argc, char* argv[])
+#include <iostream>
+#include <memory>
+
+int
+parse(int argc, const char* argv[])
 {
   try
   {
-    cxxopts::Options options(argv[0], " - example command line options");
-    options.positional_help("[optional args]");
+    std::unique_ptr<cxxopts::Options> allocated(new cxxopts::Options(argv[0], " - example command line options"));
+    auto& options = *allocated;
+    options
+      .positional_help("[optional args]")
+      .show_positional_help();
 
     bool apple = false;
 
-    options.add_options()
-      ("a,apple", "an apple", cxxopts::value<bool>(apple))
+    options
+      .set_width(70)
+      .set_tab_expansion()
+      .allow_unrecognised_options()
+      .add_options()
+      ("a,apple,ringo", "an apple", cxxopts::value<bool>(apple))
       ("b,bob", "Bob")
+      ("char", "A character", cxxopts::value<char>())
+      ("t,true", "True", cxxopts::value<bool>()->default_value("true"))
       ("f, file", "File", cxxopts::value<std::vector<std::string>>(), "FILE")
       ("i,input", "Input", cxxopts::value<std::string>())
       ("o,output", "Output file", cxxopts::value<std::string>()
           ->default_value("a.out")->implicit_value("b.def"), "BIN")
+      ("x", "A short-only option", cxxopts::value<std::string>())
       ("positional",
         "Positional arguments: these are the arguments that are entered "
         "without an option", cxxopts::value<std::vector<std::string>>())
       ("long-description",
         "thisisareallylongwordthattakesupthewholelineandcannotbebrokenataspace")
       ("help", "Print help")
+      ("tab-expansion", "Tab\texpansion")
       ("int", "An integer", cxxopts::value<int>(), "N")
       ("float", "A floating point number", cxxopts::value<float>())
+      ("vector", "A list of doubles", cxxopts::value<std::vector<double>>())
       ("option_that_is_too_long_for_the_help", "A very long option")
+      ("l,list", "List all parsed arguments (including default values)")
+      ("range", "Use range-for to list arguments")
     #ifdef CXXOPTS_USE_UNICODE
       ("unicode", u8"A help option with non-ascii: à. Here the size of the"
         " string should be correct")
@@ -63,28 +78,49 @@ int main(int argc, char* argv[])
 
     options.parse_positional({"input", "output", "positional"});
 
-    options.parse(argc, argv);
+    auto result = options.parse(argc, argv);
 
-    if (options.count("help"))
+    if (result.count("help"))
     {
       std::cout << options.help({"", "Group"}) << std::endl;
-      exit(0);
+      return true;
+    }
+
+    if(result.count("list"))
+    {
+      if(result.count("range"))
+      {
+        for(const auto &kv: result)
+        {
+          std::cout << kv.key() << " = " << kv.value() << std::endl;
+        }
+      }
+      else
+      {
+        std::cout << result.arguments_string() << std::endl;
+      }
+      return true;
     }
 
     if (apple)
     {
-      std::cout << "Saw option ‘a’ " << options.count("a") << " times " <<
+      std::cout << "Saw option ‘a’ " << result.count("a") << " times " <<
         std::endl;
     }
 
-    if (options.count("b"))
+    if (result.count("b"))
     {
       std::cout << "Saw option ‘b’" << std::endl;
     }
 
-    if (options.count("f"))
+    if (result.count("char"))
     {
-      auto& ff = options["f"].as<std::vector<std::string>>();
+      std::cout << "Saw a character ‘" << result["char"].as<char>() << "’" << std::endl;
+    }
+
+    if (result.count("f"))
+    {
+      auto& ff = result["f"].as<std::vector<std::string>>();
       std::cout << "Files" << std::endl;
       for (const auto& f : ff)
       {
@@ -92,44 +128,74 @@ int main(int argc, char* argv[])
       }
     }
 
-    if (options.count("input"))
+    if (result.count("input"))
     {
-      std::cout << "Input = " << options["input"].as<std::string>()
+      std::cout << "Input = " << result["input"].as<std::string>()
         << std::endl;
     }
 
-    if (options.count("output"))
+    if (result.count("output"))
     {
-      std::cout << "Output = " << options["output"].as<std::string>()
+      std::cout << "Output = " << result["output"].as<std::string>()
         << std::endl;
     }
 
-    if (options.count("positional"))
+    if (result.count("positional"))
     {
       std::cout << "Positional = {";
-      auto& v = options["positional"].as<std::vector<std::string>>();
+      auto& v = result["positional"].as<std::vector<std::string>>();
       for (const auto& s : v) {
         std::cout << s << ", ";
       }
       std::cout << "}" << std::endl;
     }
 
-    if (options.count("int"))
+    if (result.count("int"))
     {
-      std::cout << "int = " << options["int"].as<int>() << std::endl;
+      std::cout << "int = " << result["int"].as<int>() << std::endl;
     }
 
-    if (options.count("float"))
+    if (result.count("float"))
     {
-      std::cout << "float = " << options["float"].as<float>() << std::endl;
+      std::cout << "float = " << result["float"].as<float>() << std::endl;
+    }
+
+    if (result.count("vector"))
+    {
+      std::cout << "vector = ";
+      const auto values = result["vector"].as<std::vector<double>>();
+      for (const auto& v : values) {
+        std::cout << v << ", ";
+      }
+      std::cout << std::endl;
     }
 
     std::cout << "Arguments remain = " << argc << std::endl;
 
-  } catch (const cxxopts::OptionException& e)
+    auto arguments = result.arguments();
+    std::cout << "Saw " << arguments.size() << " arguments" << std::endl;
+
+    std::cout << "Unmatched options: ";
+    for (const auto& option: result.unmatched())
+    {
+      std::cout << "'" << option << "' ";
+    }
+    std::cout << std::endl;
+  }
+  catch (const cxxopts::exceptions::exception& e)
   {
     std::cout << "error parsing options: " << e.what() << std::endl;
-    exit(1);
+    return false;
+  }
+
+  return true;
+}
+
+int main(int argc, const char* argv[])
+{
+  if (!parse(argc, argv))
+  {
+    return 1;
   }
 
   return 0;
