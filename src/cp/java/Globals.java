22ca198cdb6f47ac08321f3073b10bac2f5bd53f
ZHU JUNHAO <realzhujunhao@gmail.com>
2023-11-21 11:24:43 +0000

    Globals.java

--- src/cp/java/Globals.java
+++ src/cp/java/Globals.java
@@ -3,5 +3,5 @@ package cp.java;
 public final class Globals {
   public static final int QUEUE_SIZE = 100;
   public static final int TUNING_KNOB = 800; // Default size of a bucket
-  public static final String DG_FETCHER_URL = "cp.elc.cs.yonsei.ac.kr:9105";
+  public static final String DG_FETCHER_URL = "10.140.2.1:9105";
 }
