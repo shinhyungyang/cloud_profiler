89e4d4d4754fdeca4b8a7381b817e8a383b4efa5
Shinhyung Yang <shinhyung.yang@email.uni-kiel.de>
2024-08-05 17:21:24 +0200

    Modified the cmake module finding rule

--- cmake/Modules/FindSQUASH.cmake
+++ cmake/Modules/FindSQUASH.cmake
@@ -4,12 +4,12 @@ set(SQUASH_PREFIX "" CACHE PATH "The path to the prefix of a SQUASH installation
 find_library(SQUASH_LIBRARY NAMES squash0.8
     HINTS ${SQUASH_PREFIX} ENV LD_LIBRARY_PATH
     #PATHS /opt/squash/0.7/lib /usr/local
-    PATHS /opt/squash/0.8/lib /usr/local
+    PATHS ${SQUASH_ROOT}/lib /opt/squash/0.8/lib /usr/local
     PATH_SUFFIXES lib)
 find_path(SQUASH_INCLUDE_DIR squash.h squash
     HINTS ${SQUASH_PREFIX}
     #PATHS /opt/squash/0.7/include/squash-0.7 /usr/local
-    PATHS /opt/squash/0.8/include/squash-0.8 /usr/local
+    PATHS ${SQUASH_ROOT}/include/squash-0.8 /opt/squash/0.8/include/squash-0.8 /usr/local
     PATH_SUFFIXES squash-0.8)
 
 message(STATUS "${SQUASH_LIBRARY}, ${SQUASH_INCLUDE_DIR}")
